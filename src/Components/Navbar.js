779eb0f9e49807662da9e5523bd7b05fcdf62453
github username <github email address>
2017-06-27 10:17:10 -0400

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavBar(){
+  return (
+  <nav className="navbar navbar-inverse bg-inverse">
+  <h2 className="white">The Gathering</h2>
+</nav>
+)
+}
